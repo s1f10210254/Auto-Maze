2ad14704a0a9d1b9be1dd415c96c6b58aa8a5d07
s1f10210254 <iniad@DESKTOP-LC3J6L9.localdomain>
2023-07-18 14:40:28 +0900

    探索×

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,79 +3,147 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [maze, setMaze] = useState([
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
   ]);
 
+  const [human, setHuman] = useState({
+    x: 0,
+    y: 0,
+    front: [0, -1],
+  });
+
   const front = [
+    //上右下左
     [0, 1],
     [1, 0],
     [0, -1],
     [-1, 0],
   ];
 
-  const mazeGeneration = () => {
+  const mazeGeneration_Odd = () => {
+    const startCells: number[][] = [];
+
     for (let x = 0; x < maze.length; x++) {
       for (let y = 0; y < maze[x].length; y++) {
         if (x % 2 === 1 && y % 2 === 1) {
+          startCells.push([x, y]);
           const updatedMaze = [...maze];
           updatedMaze[x][y] = 1;
           setMaze(updatedMaze);
-          console.table(maze);
         }
       }
     }
-
-    const startCells = [];
-    for (let i = 0; i < maze.length; i++) {
-      for (let j = 0; j < maze[i].length; j++) {
-        if (maze[i][j] === 1) {
-          startCells.push([i, j]);
-        }
-      }
-    }
-    const randomStartCell = startCells[Math.floor(Math.random() * startCells.length)];
-    const randomDirection = front[Math.floor(Math.random() * front.length)];
-
-    // 選んだセルの周囲のセルを計算
-    const x = randomStartCell[0] + randomDirection[0];
-    const y = randomStartCell[1] + randomDirection[1];
-
-    // そのセルが範囲内であれば、そのセルの値を1に変更
-    if (x >= 0 && x < maze.length && y >= 0 && y < maze[0].length) {
-      const newMaze = [...maze];
-      newMaze[x][y] = 1;
-      setMaze(newMaze);
-    }
+    console.log('両方奇数を１にする↓');
+    console.table(maze);
+    return startCells;
   };
 
-  // const generation = () => {
-  //   for (let x = 0; x < maze.length; x++) {
-  //     for (let y = 0; y < maze[x].length; y++) {
-  //       if (x % 2 === 1 && y % 2 === 1) {
-  //         const updatedMaze = [...maze];
-  //         updatedMaze[x][y] = 1;
-  //         setMaze(updatedMaze);
-  //         console.table(maze);
-  //       }
-  //     }
-  //   }
-  // };
+  const generation = () => {
+    const startCells = mazeGeneration_Odd();
+    const updatedMaze = [...maze];
+    for (const startCell of startCells) {
+      const [x, y] = startCell;
+
+      // ランダムな方向を選択
+      const randomDirectionIndex = Math.floor(Math.random() * 4);
+      const randomDirection = front[randomDirectionIndex];
+      const [dx, dy] = randomDirection;
+
+      // 選択した方向のセルが迷路の範囲内であれば、そのセルの値を1に変更
+      const newX = x + dx;
+      const newY = y + dy;
+      if (newX >= 0 && newX < maze.length && newY >= 0 && newY < maze[0].length) {
+        updatedMaze[newX][newY] = 1;
+      }
+    }
+
+    setMaze(updatedMaze);
+    console.log('迷路盤↓');
+    console.table(maze);
+  };
+
+  const leftMove = () => {
+    const { x, y, front } = human;
+    const newX = x + front[1];
+    const newY = y - front[0];
+
+    setHuman({ x: newX, y: newY, front: [-1, 0] });
+  };
+
+  const downMove = () => {
+    const { x, y, front } = human;
+    const newX = x + front[0];
+    const newY = y + front[1];
+
+    setHuman({ x: newX, y: newY, front: [0, -1] });
+  };
+
+  const rightRotation = () => {
+    const { front } = human;
+    const newFront = [front[1], -front[0]];
+
+    setHuman((prevHuman) => ({
+      ...prevHuman,
+      front: newFront,
+    }));
+  };
+
+  const moveHuman = () => {
+    const { x, y, front } = human;
+
+    // 左手、前方の座標を計算
+    const leftHandX = x + front[1];
+    const leftHandY = y - front[0];
+    const frontX = x + front[0];
+    const frontY = y + front[1];
+
+    // 左手が壁でない場合
+    if (
+      leftHandX >= 0 &&
+      leftHandX < maze.length &&
+      leftHandY >= 0 &&
+      leftHandY < maze[0].length &&
+      maze[leftHandX][leftHandY] === 0
+    ) {
+      leftMove();
+    }
+    // 左手が壁で前が空いている場合
+    else if (
+      maze[x][y] === 0 &&
+      frontX >= 0 &&
+      frontX < maze.length &&
+      frontY >= 0 &&
+      frontY < maze[0].length &&
+      maze[frontX][frontY] === 0
+    ) {
+      downMove();
+    }
+    // 左手が壁でかつ前も壁の場合
+    else {
+      rightRotation();
+    }
+    console.log(human);
+  };
 
   return (
     <div className={styles.container}>
-      {['生成'].map((v) => (
-        <li onClick={mazeGeneration} key={v}>
-          {v}
-        </li>
-      ))}
+      <button className={styles.button} onClick={generation}>
+        生成
+      </button>
+      <button className={styles.human} onClick={moveHuman}>
+        探索
+      </button>
 
       <div className={styles.maze}>
         {maze.map((row, x) =>
           row.map((cell, y) => (
             <div className={styles.cell} key={`${x}-${y}`}>
               {cell === 1 && <div className={styles.pillar} />}
+              {human.x === x && human.y === y && <div className={styles.position} />}
             </div>
           ))
         )}

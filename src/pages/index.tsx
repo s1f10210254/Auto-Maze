691a18258175af77c4ce8d639f261d395a4a25bb
s1f10210254 <iniad@DESKTOP-LC3J6L9.localdomain>
2023-07-22 13:34:53 +0900

    矢印追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,26 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [maze, setMaze] = useState([
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
+  // console.log('xy確認', maze[1][3]);
+
   const [human, setHuman] = useState({
-    x: 4,
-    y: 4,
-    front: [1, 0],
+    x: 0,
+    y: 0,
+    front: [-1, 0],
   });
 
-  const front = [
+  const directions = [
     //上右下左
     [0, 1],
     [1, 0],
@@ -50,7 +56,7 @@ const Home = () => {
 
       // ランダムな方向を選択
       const randomDirectionIndex = Math.floor(Math.random() * 4);
-      const randomDirection = front[randomDirectionIndex];
+      const randomDirection = directions[randomDirectionIndex];
       const [dy, dx] = randomDirection;
 
       // 選択した方向のセルが迷路の範囲内であれば、そのセルの値を1に変更
@@ -70,69 +76,97 @@ const Home = () => {
 
   const leftMove = () => {
     const { x, y, front } = human;
-    // const newFront = [front[0] - 1, front[1] - 1];
-    // setHuman((prevHuman) => ({
-    //   ...prevHuman,
-    //   front: newFront,
-    // }));
-    const nextX = x + front[0];
-    const nextY = y + front[1];
-    setHuman((prevHuman) => ({
-      ...prevHuman,
-      x: nextX,
-      y: nextY,
-    }));
-  };
+    const [dx, dy] = front;
 
-  // const downMove = () => {
-  //   const { x, y, front } = human;
-  //   const nextX = x + front[0];
-  //   const nextY = y + front[1];
+    // 現在の向きを左に90度変更
+    const newFront = [dy, -dx];
 
-  //   setHuman((prevHuman) => ({
-  //     ...prevHuman,
-  //     x: nextX,
-  //     y: nextY,
-  //   }));
-  // };
+    // 新しい座標を計算
+    const nextX = x + newFront[0];
+    const nextY = y + newFront[1];
 
-  // const rightRotation = () => {
-  //   const { front } = human;
-  //   const newFront = [front[0] + 1, front[1] + 1];
-
-  //   setHuman((prevHuman) => ({
-  //     ...prevHuman,
-  //     front: newFront,
-  //   }));
-  // };
-
-  const moveHuman = () => {
-    const { x, y, front } = human;
-    const newFront = [front[0] - 1, front[1] - 1];
-    setHuman((prevHuman) => ({
-      ...prevHuman,
-      front: newFront,
-    }));
-    const nextX = x + front[1];
-    const nextY = y + front[0];
-    console.log('newFront', newFront);
-    console.log('人間の次のX座標', nextX, '人間の次のY座標', nextY);
-    const setFront = [front[1], front[0]];
-    setHuman((prevHuman) => ({
-      ...prevHuman,
-      front: setFront,
-    }));
+    // 新しい座標が迷路の範囲内であり、かつ壁がない場合は前進
     if (
       nextX >= 0 &&
       nextX < maze.length &&
       nextY >= 0 &&
       nextY < maze[0].length &&
-      maze[nextY][nextX] === 0
+      maze[nextY][nextX] !== 1
+    ) {
+      setHuman({ x: nextX, y: nextY, front: newFront });
+    }
+  };
+
+  const goMove = () => {
+    const { x, y, front } = human;
+    const [dx, dy] = front;
+
+    // 現在の座標の前方に進む座標を計算
+    const nextX = x + dx;
+    const nextY = y + dy;
+
+    // 新しい座標が迷路の範囲内であり、かつ壁がない場合は前進
+    if (
+      nextX >= 0 &&
+      nextX < maze.length &&
+      nextY >= 0 &&
+      nextY < maze[0].length &&
+      maze[nextY][nextX] !== 1
+    ) {
+      setHuman({ ...human, x: nextX, y: nextY });
+    }
+  };
+
+  const turnleft = () => {
+    const { front } = human;
+    const [dx, dy] = front;
+
+    // 現在の向きを右に90度変更
+    const newFront = [dy, -dx];
+    setHuman({ ...human, front: newFront });
+  };
+
+  const moveHuman = () => {
+    const { x, y, front } = human;
+    const [dx, dy] = front;
+
+    // 左手が壁でない場合は左に回る
+    // const leftX = x - dy;
+    // const leftY = y + dx;
+    const leftX = x + dy;
+    const leftY = y - dx;
+    if (
+      leftX >= 0 &&
+      leftX < maze.length &&
+      leftY >= 0 &&
+      leftY < maze[0].length &&
+      maze[leftY][leftX] === 1
     ) {
       leftMove();
-      console.log('leftMove実行');
+      console.log('leftmove実行');
     }
-    console.log(human);
+    // 前が壁でない場合は前に進む
+    else if (
+      x + dx >= 0 &&
+      x + dx < maze.length &&
+      y + dy >= 0 &&
+      y + dy < maze[0].length &&
+      maze[y + dy][x + dx] !== 1
+    ) {
+      goMove();
+      console.log('goMove実行');
+    }
+    // どちらも壁の場合は右に回る
+    else {
+      turnleft();
+      console.log('turnleft実行');
+    }
+  };
+
+  // 矢印の向きを計算する関数
+  const getArrowRotation = (dx: number, dy: number): number => {
+    const angle = Math.atan2(dy, dx);
+    return (angle * 180) / Math.PI;
   };
 
   return (
@@ -150,7 +184,16 @@ const Home = () => {
           row.map((cell, y) => (
             <div className={styles.cell} key={`${x}-${y}`}>
               {cell === 1 && <div className={styles.pillar} />}
-              {human.x === x && human.y === y && <div className={styles.position} />}
+              {human.x === x && human.y === y && (
+                <div className={styles.position}>
+                  <div
+                    className={styles.arrow}
+                    style={{
+                      transform: `rotate(${getArrowRotation(human.front[0], human.front[1])}deg)`,
+                    }}
+                  />
+                </div>
+              )}
             </div>
           ))
         )}
